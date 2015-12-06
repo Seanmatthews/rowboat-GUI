e61c78d1b43fee82536a4bf299478d01a1a74db4
Sean <sean.d.matthews@gmail.com>
2015-12-06 00:40:41 -0500

    removes ignore files

--- operator/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /vagrant/operator/src
-# Build directory: /vagrant/operator/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(gtest)
-SUBDIRS(gscam)
--- operator/build/gscam/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /vagrant/operator/src/gscam
-# Build directory: /vagrant/operator/build/gscam
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
